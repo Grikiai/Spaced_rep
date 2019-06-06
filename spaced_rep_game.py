4f2d5127d56f868c393e4384d26d67590abbad1a
Grikiai <51445984+Grikiai@users.noreply.github.com>
2019-06-06 19:17:41 +0300

    Update spaced_rep_game.py

--- spaced_rep_game.py
+++ spaced_rep_game.py
@@ -26,43 +26,67 @@ x = GameDay(startdate, datenow)
 
 # sudedu 1, 2 ir 3 i zodynus
 zodziai1 = open('level_1.txt', 'r')
-    Wordlist1 = zodziai1.readlines()
-    zodziai1.close()
-    first_box = {}
-    for i in Wordlist1:
-      key = i.split('-')[0].strip()
-      if key not in first_box:
-      first_box[key] = i.split('-')[1].strip()
-    zodziai2 = open('level_2.txt', 'r')
-    Wordlist2 = zodziai2.readlines()
-    zodziai2.close()
-    second_box = {}
-    for i in Wordlist2:
-      key = i.split('-')[0].strip()
-      if key not in second_box:
-      second_box[key] = i.split('-')[1].strip()
-    zodziai3 = open('level_3.txt', 'r')
-    Wordlist3 = zodziai3.readlines()
-    zodziai2.close()
-    third_box = {}
-    for i in Wordlist3:
-      key = i.split('-')[0].strip()
-      if key not in third_box:
-      third_box[key] = i.split('-')[1].strip()  
+Wordlist1 = zodziai1.readlines()
+zodziai1.close()
+first_box = {}
+for i in Wordlist1:
+  key = i.split('-')[0].strip()
+  if key not in first_box:
+    first_box[key] = i.split('-')[1].strip()
+    
+zodziai2 = open('level_2.txt', 'r')
+Wordlist2 = zodziai2.readlines()
+zodziai2.close()
+second_box = {}
+for i in Wordlist2:
+  key = i.split('-')[0].strip()
+  if key not in second_box:
+    second_box[key] = i.split('-')[1].strip()
+    
+zodziai3 = open('level_3.txt', 'r')
+Wordlist3 = zodziai3.readlines()
+zodziai2.close()
+third_box = {}
+for i in Wordlist3:
+  key = i.split('-')[0].strip()
+  if key not in third_box:
+    third_box[key] = i.split('-')[1].strip()  
       
       
 class Card:
-  def __init__(self, pol, ltu):
+  def __init__(self, pol, ltu, correct = True):
     self.lenkiskai = pol
     self.lietuviskai = ltu
-class Box:
-  def sort_cards(self):
-  def 
+    self.correct = correct
     
-if x.get_day()%3 == 0 and x.get_day()%2 == 0:  
-elif x.get_day()%2 == 0   
+  def ask_in_polish(self):
+    print('Whats Lithuanian for '+ self.lenkiskai+ '?')
+    answer = input()
+    if answer.lower() == self.lietuviskai:
+      self.correct == True
+    else:
+      self.correct == False
+  
+class Box:
+  def __init__(self):
+  
+  def get_first_box(self):
+    
+  def get_second_box(self):
+    
+  def get_third_box(self):  
+
+
+if x.get_day()%3 == 0 and x.get_day()%2 == 0:
+  
+elif x.get_day()%2 == 0
+
 elif x.get_day()%3 == 0:
+  
     # sudedu 1, 2 ir 3 i zodynus
     
+    # reikia dar duoti user inputa
+    # apkeist dezes
+    # irasyti dezes i failus atgal
     
    
