956dcb66ac7a6b1edc599ac1a7827aaa1af4bcf1
Grikiai <51445984+Grikiai@users.noreply.github.com>
2019-06-06 18:48:48 +0300

    Update spaced_rep_game.py

--- spaced_rep_game.py
+++ spaced_rep_game.py
@@ -1,27 +1,68 @@
-x = 'level_1.txt'
-y = 'level_2.txt'
-z = 'level_3.txt'
+# suloginu
+import datetime
+inDate = datetime.datetime.now()
+log = open('log.txt', 'a')
+log.write(inDate)
+log.close()
 
-
-for key in dictio:
-  print('how is '+dictio[key]+' in lithuanian?')
-  x = input()
-  if x.lower()  == dictio[key]:
-    print('you guessed correctly!!')
-    dictio[key]='1'
-  else:
-    print('you guessed wrong :( the correct answer is '+ dictio[key])
-    dictio[key]='0'
-
-class Card:
-  def __init__(self, dictio.keys(), dictio.values()):
-    self.ques = dictio.keys()
-    self.ans = dictio.values()
-
-class Box:
-  def __init__(self, nr, cards):
-    self.nr = nr
-    self.cards = cards
-
-  def sort_cards(self):
+class GameDay:
+  def __init__(self, startdate, datenow):
+    self.startdate = startdate
+    self.datenow = datenow
     
+  def get_day(self):
+    Scheduled = int(self.dateNow.strftime('%j'))-int(self.startDate.strftime('%j'))+1
+    box_seq = DateScheduling[Scheduled-1]
+    return box_seq
+
+# datos kintamasis
+dates = open ( 'log.txt',"r" )
+DateList = dates.readlines()
+dates.close()
+startdate = DateList[0]
+datenow = DateList[-1]
+
+x = GameDay(startdate, datenow)
+
+# sudedu 1, 2 ir 3 i zodynus
+zodziai1 = open('level_1.txt', 'r')
+    Wordlist1 = zodziai1.readlines()
+    zodziai1.close()
+    first_box = {}
+    for i in Wordlist1:
+      key = i.split('-')[0].strip()
+      if key not in first_box:
+      first_box[key] = i.split('-')[1].strip()
+    zodziai2 = open('level_2.txt', 'r')
+    Wordlist2 = zodziai2.readlines()
+    zodziai2.close()
+    second_box = {}
+    for i in Wordlist2:
+      key = i.split('-')[0].strip()
+      if key not in second_box:
+      second_box[key] = i.split('-')[1].strip()
+    zodziai3 = open('level_3.txt', 'r')
+    Wordlist3 = zodziai3.readlines()
+    zodziai2.close()
+    third_box = {}
+    for i in Wordlist3:
+      key = i.split('-')[0].strip()
+      if key not in third_box:
+      third_box[key] = i.split('-')[1].strip()  
+      
+      
+class Card:
+  def __init__(self, pol, ltu):
+    self.lenkiskai = pol
+    self.lietuviskai = ltu
+class Box:
+  def sort_cards(self):
+  def 
+    
+if x.get_day()%3 == 0 and x.get_day()%2 == 0:  
+elif x.get_day()%2 == 0   
+elif x.get_day()%3 == 0:
+    # sudedu 1, 2 ir 3 i zodynus
+    
+    
+   
